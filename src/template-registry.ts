c5d7e826905c801b305316e0325a3ed4d108019a
Matthew Irish <matthew.irish@gmail.com>
2026-03-19 23:35:28 -0500

    actually add components to the template registry

--- src/template-registry.ts
+++ src/template-registry.ts
@@ -2,9 +2,11 @@
 // Add all your components, helpers and modifiers to the template registry here, so apps don't have to do this.
 // See https://typed-ember.gitbook.io/glint/environments/ember/authoring-addons
 
-// import type MyComponent from './components/my-component';
+import type Outlet from './components/outlet';
+import type RoutePortal from './components/route-portal';
 
 // Uncomment this once entries have been added! 👇
-// export default interface Registry {
-//   MyComponent: typeof MyComponent
-// }
+export default interface Registry {
+  Outlet: typeof Outlet,
+  RoutePortal: typeof RoutePortal
+}
