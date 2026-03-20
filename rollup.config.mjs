a487eed8d85d3a9bae4049935715d3ab8b842dc1
Matthew Irish <matthew.irish@gmail.com>
2026-03-19 22:19:44 -0500

    update config files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,9 +8,6 @@ const addon = new Addon({
   destDir: 'dist',
 });
 
-const rootDirectory = dirname(fileURLToPath(import.meta.url));
-const babelConfig = resolve(rootDirectory, './babel.publish.config.cjs');
-
 export default {
   // This provides defaults that work well alongside `publicEntrypoints` below.
   // You can augment this if you need to.
@@ -24,7 +21,8 @@ export default {
     // up your addon's public API. Also make sure your package.json#exports
     // is aligned to the config here.
     // See https://github.com/embroider-build/embroider/blob/main/docs/v2-faq.md#how-can-i-define-the-public-exports-of-my-addon
-    addon.publicEntrypoints(['index.ts', 'routes.ts', 'components/**/*.gts']),
+    addon.publicEntrypoints(['**/*.js', 'index.js', 'template-registry.js']),
+
 
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
@@ -49,9 +47,7 @@ export default {
     // babel.config.json.
     babel({
       extensions: ['.js', '.gjs', '.ts', '.gts'],
-      exclude: ['**/*.d.ts'],
       babelHelpers: 'bundled',
-      configFile: babelConfig,
     }),
 
     // Ensure that standalone .hbs files are properly integrated as Javascript.
@@ -59,6 +55,9 @@ export default {
 
     // Ensure that .gjs files are properly integrated as Javascript
     addon.gjs(),
+
+    // Emit .d.ts declaration files
+    addon.declarations('declarations'),
 
     // addons are allowed to contain imports of .css files, which we want rollup
     // to leave alone and keep in the published output.
